33218dcf80d8fb70d85a184b5459d9a055a0745e
DanCvejn <70745296+DanCvejn@users.noreply.github.com>
2023-06-13 11:37:26 +0200

    little fix

--- src/components/content/Container.jsx
+++ src/components/content/Container.jsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 const Container = ({ children }) => {
+  useEffect(() => {
+    window.scrollTo(0,0);
+  }, []);
+
   return (
     <div className="container px-4">
       {children}
